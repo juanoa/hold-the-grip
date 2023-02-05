33c2ca65fa8c88c338e57bc07a593eb5a7d0d05d
Juan Otalora <juanoa.otalora@gmail.com>
2023-02-05 19:14:05 +0100

    Add tag as badge

--- src/components/pages/routes/route-page/TagsSecion.jsx
+++ src/components/pages/routes/route-page/TagsSecion.jsx
@@ -1,10 +1,11 @@
 import style from "@/styles/pages/routes/RoutePage.module.css";
+import {Badge} from "theme-ui";
 
 export const TagsSecion = ({tags}) => {
   return (
     <section className={style.tagsSection}>
       {
-        tags.map((tag, i) => <span key={i}>{tag} </span>)
+        tags.map((tag, i) => <Badge key={i}>{tag.toUpperCase()}</Badge>)
       }
     </section>
   )
